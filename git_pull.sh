2cee5b3f52f50d89c79ce84671b2735df9c97096
Zach Gray <zgray@avantia-inc.com>
2015-01-25 03:41:20 -0500

    update commenting

--- git_pull.sh
+++ git_pull.sh
@@ -7,6 +7,8 @@ echo "Pulling latest code..."
 git reset --hard HEAD
 git pull
 
+# assuming you're using node-dev as recommended, the server should restart as needed to pick up changes
+
 # if you're using git submodules, uncomment this line to ensure you get latest.
 # (newer git does this by default - older git does not)
 # git submodule update --init --recursive
